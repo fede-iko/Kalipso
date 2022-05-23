066e69a8ed6ada05d5b1240a30bc0ce5ceb90232
Cristian Mannella <cristianmannellapc@gmail.com>
2022-05-23 23:38:32 +0200

    Few Changes
    Completed DBConnectionTests v:1.0.0
    Added the MySQL driver to the application folder.
    Fixed some attributes names.
    Started implementing Answer class.

--- java/java_application/src/application/Answer.java
+++ java/java_application/src/application/Answer.java
@@ -2,4 +2,29 @@ package application;
 
 public class Answer {
 
+	private short id_sentence;
+	private String answer_text;
+	private boolean isCorrect;
+	
+	public Answer(short id_sentence,String answer_text,boolean isCorrect) {
+		this.id_sentence = id_sentence;
+		this.answer_text = answer_text;
+		this.isCorrect = isCorrect;
+	}
+	
+	public short getIdSentence() {
+		return this.id_sentence;
+	}
+	
+	public String getAnswerText() {
+		return this.answer_text;
+	}
+	
+	public boolean getIsCorrect() {
+		return this.isCorrect;
+	}
+	
+	
+	
+	
 }
--- java/java_application/src/tests/DBConnectionTests.java
+++ java/java_application/src/tests/DBConnectionTests.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.SQLException;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
@@ -15,7 +17,26 @@ public class DBConnectionTests {
 	DBConnection db = null;
 
 	public DBConnection getValidConnection() throws SQLException {		
-		return new DBConnection();
+		return new DBConnection();		
+	}
+	
+	@BeforeEach
+	void setUp() {
+		try {
+			db = getValidConnection();
+		} catch (SQLException e) {
+			System.out.println("Cannot open connection!");
+		}
+	}
+	
+	@AfterEach
+	void tearDown() {
+		try {
+			db.getConnection().close();
+			db = null;
+		} catch (SQLException e) {
+			System.out.println("Cannot close connection!");
+		}
 	}
 	
 	@Test
@@ -37,30 +58,51 @@ public class DBConnectionTests {
 	
 	@Test
 	public void good_select_statement() throws SQLException {
-		String query = "SELECT * FROM sentence";
+		String query = "SELECT * FROM sentence";		
 		
-		DBConnection db1 = getValidConnection();
-		
-		db1.selectQuery(query); //If the query is not valid then it will throws the exception
+		db.selectQuery(query); //If the query is not valid then it will throws the exception
 	}
 	
 	@Test
 	public void wrong_select_statement() throws SQLException {
 		String query = "SELECT * FROM wrong_table WHERE wrong_field = 1";
 		
-		DBConnection db = getValidConnection();
 		assertThrows(SQLException.class,
 				() ->{
 					db.selectQuery(query);
 				});
 	}
 	
-	protected void tearDown() {
-		try {
-			db.getConnection().close();
-		} catch (SQLException e) {
-			System.out.println("Cannot close connection!");
-		}
+	@Test
+	public void good_update() throws SQLException{
+		
+		String query = "UPDATE sentence SET n_total = n_total + 1 WHERE id_sentence = 1;";
+		
+		assertTrue(db.updateQuery(query));		
 	}
+	
+	@Test
+	public void wrong_update() throws SQLException{
+		
+		String query = "UPDATE flowers SET n_total = n_total + 1 WHERE id_sentence = 1;";		
+		
+		assertFalse(db.updateQuery(query));		
+	}
+	
+	@Test
+	public void good_delete() throws SQLException{
+		
+		String query = "DELETE FROM sentence WHERE id_sentence = 1;";
+		
+		assertTrue(db.deleteQuery(query));		
+	}
+	
+	@Test
+	public void wrong_delete() throws SQLException{
+		
+		String query = "DELETE FROM roses WHERE id_sentence = 1213;";
+		
+		assertTrue(db.deleteQuery(query));
+	}	
 	
 }
