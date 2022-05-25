83dc71f6e2805d6acc4783fd09e9a808e9704708
BothBasilisk <100195778+BothBasilisk@users.noreply.github.com>
2022-05-25 16:43:27 +0200

    Classe SentencesContainer completata

--- java/java_application/src/application/SentencesContainer.java
+++ java/java_application/src/application/SentencesContainer.java
@@ -1,5 +1,43 @@
 package application;
 
-public class SentencesContainer {
+import java.util.Collections;
+import java.util.List;
 
+public class SentencesContainer {
+	private List<Sentence> sentences;
+	
+	public SentencesContainer() {
+		
+	}
+	
+	public boolean addSentence(Sentence s) {
+		if(s!=null) {
+			this.sentences.add(s);
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+	
+	public boolean shuffleSentences() {
+		
+		if(this.sentences.size() >= 3) {
+			Collections.shuffle(this.sentences);
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public Sentence getSentence(int index) {
+		Sentence sentence = new Sentence();
+		try {
+			sentence = this.sentences.get(index);
+		} catch (IndexOutOfBoundsException e) {
+			throw e;
+		}
+		return sentence;
+		
+	}
 }
