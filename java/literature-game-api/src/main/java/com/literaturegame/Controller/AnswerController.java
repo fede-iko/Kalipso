1752f4496f879340fed54cf0e7f406ed60e13983
BothBasilisk <100195778+BothBasilisk@users.noreply.github.com>
2022-05-26 12:43:01 +0200

    update autowired

--- java/literature-game-api/src/main/java/com/literaturegame/Controller/AnswerController.java
+++ java/literature-game-api/src/main/java/com/literaturegame/Controller/AnswerController.java
@@ -1,7 +1,23 @@
 package com.literaturegame.Controller;
 
+import com.literaturegame.Entity.Answer;
+import com.literaturegame.Repository.AnswerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class AnswerController {
+
+    AnswerRepository answerRepository;
+
+    @CrossOrigin
+    @GetMapping("/answer")
+    public List<Answer> findAnswers(){
+        return answerRepository.findAll();
+    }
+
 }
