3d300be9856f5476970fa5431d982d3435b9da20
Cristian Mannella <cristianmannellapc@gmail.com>
2022-05-26 11:24:02 +0200

    Added API folder

---
+++ java/literature-game-api/src/main/java/com/literaturegame/literaturegame/LiteratureGameApplication.java
@@ -0,0 +1,14 @@
+package com.literaturegame.literaturegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class LiteratureGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteratureGameApplication.class, args);
+	}
+
+}
