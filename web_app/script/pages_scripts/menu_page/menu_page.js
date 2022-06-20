ed53aa84a64e8b0360dbb1b05692f6640a3da6b5
Cristian Mannella <cristianmannellapc@gmail.com>
2022-06-20 22:06:41 +0200

    Improved settings and instructions

--- web_app/script/pages_scripts/menu_page/menu_page.js
+++ web_app/script/pages_scripts/menu_page/menu_page.js
@@ -1,35 +1,107 @@
+//SETTINGS FOR THE MENU PAGE, FOR OTHER PAGES YOU USE THE SESSION STORAGE
+var settings = {
+    bgImage: "",
+};
+
+
 $(document).ready(function() {
+
+    //IF SESSION CONTAINS THE SETTINGS, LOAD IT
+    if (sessionStorage.getItem("settings")) {
+        settings = JSON.parse(sessionStorage["settings"]);
+    }
+
+
+    //IF THE USER IS ALREADY IN THE MIDDLE OF THE GAME
     if (sessionStorage.getItem("userAnswers")) {
         $("#title").remove();
         $("#menu-container").remove();
         loadRoundsPage();
     }
 
+    //THE TITLE
     $("#title").text("Benvenuto " + sessionStorage["username"] + "!");
 
+    //GAME START BTN
     $("#start-btn").unbind().on("click", function() {
         $("#title").remove();
         $("#menu-container").remove();
         loadRoundsPage();
     });
 
+    //LOGOUT BTN
     $("#logout-btn").unbind().on("click", function() {
-        sessionStorage.removeItem("username");
+        //DELETE SESSION
+        sessionStorage.clear();
         location.reload();
     });
 
-    $("#instruction-btn").unbind().on("click", function() {
-        //OPEN MODAL
+    //SETTINGS SAVE BTN
+    $("#settingsSave").unbind().on("click", function() {
+        sessionStorage.setItem("settings", JSON.stringify(settings));
     });
 
-    $("#settings-btn").unbind().on("click", function() {
-        //OPEN MODAL
-    });
+    loadModalBackgroundsImagesChoices();
+});
 
-    $(".carousel-item").unbind().on("click", function(e) {
-        console.log(e);
-        $("body").css("background", "url(" + e.currentTarget.childNodes[1].attributes[0].nodeValue + ")");
+function loadModalBackgroundsImagesChoices() {
+    var bgsContainer = $(".bgimages-container");
+
+    for (var i = 1; i <= 8; i++) {
+
+        var bgImageContainer = $("<div></div>");
+        bgImageContainer.addClass("d-flex flex-column m-3 justify-content-center align-items-center");
+
+        var bgImageDiv = $("<div></div>");
+
+        var imageLabel = $("<label></label>");
+        imageLabel.attr("for", "bgImage" + i);
+
+        var imageUrl = "./assets/bg-images/bg" + i + ".jpg";
+
+        //IF IS THE REMOVE IMAGE BUTTON
+        if (i != 8) {
+            var image = $("<img>");
+            image.attr("src", imageUrl);
+            imageLabel.append(image);
+        } else {
+            imageLabel.text("Nessuna immagine");
+        }
+
+        bgImageDiv.append(imageLabel);
+
+        var radioDiv = $("<div></div>");
+
+        var radio = $("<input>");
+        radio.attr("type", "radio");
+        radio.attr("name", "bgImage");
+        radio.attr("id", "bgImage" + i);
+        radio.attr("value", imageUrl);
+
+
+        if (imageUrl == settings.bgImage) {
+            radio.attr("checked", "checked");
+        }
+        radio.addClass("form-check-input");
+
+        radioDiv.append(radio);
+
+        bgImageContainer.append(bgImageDiv);
+        bgImageContainer.append(radioDiv);
+
+        bgsContainer.append(bgImageContainer);
+
+        createRadioEventHandler(radio.attr("id"));
+    }
+
+}
+
+function createRadioEventHandler(id) {
+    $("#" + id).unbind().on("click", function(e) {
+        var src = $(this).val();
+        $("body").css("background", "url(" + src + ")");
         $("body").css("background-size", "cover");
         $("body").css("background-repeat", "no-repeat");
+        settings.bgImage = src;
     });
-});
+}
